7c91c38ec4e7c068080abfbd695f8a804ffd9dea
3537390 <3537390@ages-web02.geo.uu.nl>
2023-01-23 17:31:54 +0100

    updates

--- dataset/LLG/NL/form.js
+++ dataset/LLG/NL/form.js
@@ -6,32 +6,45 @@ import { default as storageHandler } from "/e107_plugins/storageHandler/js/stora
 (function () {
 
 	function dataStorage() {
-        console.log("dataStorage");
+		console.log("dataStorage");
 		const elementList = document.querySelectorAll("span[data-variable]");
 		elementList.forEach((element) => {
 			storageHandler.storage.local.set(element.dataset.variable, element.innerText)
 		});
 	}
 
-	window.Forms = [];
+	window["ajaxForms"] = [];
 
 	document.addEventListener('DOMContentLoaded', () => {
 
 		const forms = document.querySelectorAll('form[data-ajax="form"]');
 		forms.forEach((element, key) => {
-			// Check if we already have specific id in our localStorage...
-			// And if data in our localStorage is still valid...
-			// Else
-
-			var object = {
+			var formOptions = {
+				parseResponse: function (response) {
+					const type = response.type;
+					const data = response.data;
+					const dataset = response.data.dataset[0];
+					const records = data.records;
+					const totalrecords = data.totalrecords;
+					return { type, data, dataset, records, totalrecords };
+				},
 				_formCallback: {
 					functions: {
 						dataStorage
 					}
 				}
 			}
+			// Check if we already have specific id in our localStorage...
+			// And if data in our localStorage is still valid...
+			// Else
 
-			window["Forms"][key] = new ajaxForm(element, key, object);
+			// var object = {
+			// 	_formCallback: {
+			// 		functions: {
+			// 			dataStorage
+			// 		}
+			// 	}
+			window["ajaxForms"][key] = new ajaxForm(element, key, formOptions);
 			// ...and store data in our LocalStorage
 		})
 
--- dataset/LLG/NL/form.php
+++ dataset/LLG/NL/form.php
@@ -9,7 +9,7 @@ require_once($_SERVER['DOCUMENT_ROOT']."/class2.php");
 require_once(HEADERF);
 
 // ------------------------------------------------
-if(!$_GET['id'] && $_GET['action'] !== 'create') {
+if(!$_GET['borehole'] && $_GET['action'] !== 'create') {
 
     $caption = "Something went wrong";
     $text = "Click <a href='index.php'>here</a> to return to the index";
@@ -23,7 +23,7 @@ if(!$_GET['id'] && $_GET['action'] !== 'create') {
     exit;
 }
 
-if($_GET['action'] === 'update' && !$_GET['id']) {
+if($_GET['action'] === 'update' && !$_GET['borehole']) {
 
     $caption = "Something went wrong";
     $text = "Click <a href='index.php'>here</a> to return to the index";
@@ -65,29 +65,37 @@ if($_GET['action'] === 'create') {
     }
 }
 
-// ------------------------------------------------
+// --- [ API ] ------------------------------------
+$form_url = "//wikiwfs.geo.uu.nl/e107_plugins/ajaxDBQuery/server/API.php";
+$form_db = "llg";
+$form_table = "llg_nl_boreholedata";
+$form_columns = "*";
+$form_where_0_identifier = "borehole";
+$form_where_0_value = filter_var($_GET[$form_where_0_identifier], FILTER_SANITIZE_STRING);
+$form_order_by_0_identifier = "borehole";
+$form_order_by_0_direction = "DESC";
+$form_query = '{ "0": { "select": { "columns": { "0": "'.$form_columns.'" }, "from": { "table": "'.$form_table.'" } } }, "1": { "where": { "0": { "identifier": "'.$form_where_0_identifier.'", "value": "'.$form_where_0_value.'" } } }, "2": { "order_by": { "0": { "identifier": "'.$form_order_by_0_identifier.'", "direction": "'.$form_order_by_0_direction.'" } } } }';
+
+// --- [ JAVASCRIPT ] -----------------------------
 $script .= '
 <script src="./form.js" type="module">
 </script>
 ';
-// ------------------------------------------------
 
-$url = "//wikiwfs.geo.uu.nl/e107_plugins/ajaxDBQuery/server/API.php";
-$db = "rmdelta";
-$table = "cb_cat_clone";
-$columns = "*";
-$inner_join = "";
-$where_identifier = "id";
-$where_value = filter_var($_GET[$where_identifier], FILTER_SANITIZE_STRING);
-$where = "id='{$where_value}'";
-$limit = 1;
-$offset = 0;
+// --- [ TEMPLATE ] -------------------------------
 $form = '
-<form method="'.$method.'" class="container" data-ajax="form" data-url="'.$url.'" data-db="'.$db.'" data-table="'.$table.'" data-columns="'.$columns.'" data-inner_join="'.$inner_join.'" data-where="'.$where.'" data-order_by="'.$order_by.'" data-direction="'.$direction.'" data-paging="'.$paging.'" data-limit="'.$limit.'" data-offset="'.$offset.'">
-
+<form method="'.$method.'" class="container p-5"
+    data-ajax="form"
+    data-url=\''.$form_url.'\'
+    data-db=\''.$form_db.'\'
+    data-table=\''.$form_table.'\'
+    data-columns=\''.$form_columns.'\'
+    data-query=\''.$form_query.'\'>
 </form>
 ';
 
+// <form method="'.$method.'" class="container" data-ajax="form" data-url="'.$url.'" data-db="'.$db.'" data-table="'.$table.'" data-columns="'.$columns.'" data-inner_join="'.$inner_join.'" data-where="'.$where.'" data-order_by="'.$order_by.'" data-direction="'.$direction.'" data-paging="'.$paging.'" data-limit="'.$limit.'" data-offset="'.$offset.'">
+// </form>
 // ------------------------------------------------
 
 $text = $script.$form.$footerscript;
